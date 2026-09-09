6469909c7902d974953049ee7fc8b16fac669b62
mrrodriguez <mjr4184@gmail.com>
2026-09-09 11:53:56 -0500

    Add clara-explorer support

---
+++ lua/plugins/clara-explorer.lua
@@ -0,0 +1,25 @@
+local clara_root = vim.env.CLARA_HOME_EXPLORER
+
+local plugins = {
+  "Olical/conjure",
+  { "nvim-treesitter/nvim-treesitter", opts = { ensure_installed = { "clojure" } } },
+}
+
+if clara_root then
+  plugins[#plugins + 1] = {
+    dir = clara_root .. "/editor/neovim",
+    init = function()
+      vim.api.nvim_create_autocmd("FileType", {
+        pattern = "clojure",
+        callback = function(args)
+          vim.keymap.set("n", "gp", "<Cmd>ClaraExplorerNavigateProducer<CR>", { buffer = args.buf, desc = "Navigate to Producer (Clara)" })
+          vim.keymap.set("n", "gc", "<Cmd>ClaraExplorerNavigateConsumer<CR>", { buffer = args.buf, desc = "Navigate to Consumer (Clara)" })
+        end,
+      })
+    end
+  }
+else
+  vim.notify("CLARA_HOME_EXPLORER is not set — clara-explorer not loaded", vim.log.levels.WARN)
+end
+
+return plugins
